e38e367d22ec6cb56b69b092f2a94f69fa8ebb0b
Umlatt <theplacetosendmemail@gmail.com>
2022-05-30 19:18:24 +0000

    Added project path

--- app/modules/__init__.py
+++ app/modules/__init__.py
@@ -1,6 +1,9 @@
 import json
+import os
+
+project_path = "/".join(((os.path.dirname(__file__)).split("/"))[:-1])
 
 # Settings file
-#settings = json.load(open("settings.json", 'r'))
+settings = json.load(open(f"{project_path}/settings.json", 'r'))
 # Container prefix
 prefix = "peon.warcamp."
