471d23535ebd5bbe843e5be6068a35966c959753
Umlatt <theplacetosendmemail@gmail.com>
2022-05-30 13:25:15 +0000

    Added help doc

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@
 import os
 import random
 import json
+import re
 
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -12,16 +13,26 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 bot = commands.Bot(command_prefix='!')
 
 responses=json.load(open("/root/peon-bot-discord/app/documents/quotes.json"))
+with open('/root/peon-bot-discord/app/documents/help.md') as f:
+    helpText = f.readlines()
+
+usageText = (open("/root/peon-bot-discord/app/documents/help.md", "r")).read()
 
 @bot.event
 async def on_ready():
-    print(f'{bot.user.name} has connected to Discord!')
+    channels = bot.get_all_channels()
+    for channel in channels:
+        if re.search('text', str(channel.category), re.IGNORECASE):
+            if re.search('bot', str(channel.name), re.IGNORECASE):
+                await channel.send(f" has connected to the server.\n *Type ``!usage`` for furuthur information*")
+    print(f'[{bot.user.name}] has connected to Discord!')
 
 @bot.command(name='poke')
-async def nine_nine(ctx):
-    peon_quotes = responses["respond"]
+async def poke(ctx):
+    await ctx.send(random.choice(responses["respond"]))
 
-    response = random.choice(peon_quotes)
-    await ctx.send(response)
+@bot.command(name='usage')
+async def usage(ctx):
+    await ctx.send(usageText)
 
 bot.run(TOKEN)
