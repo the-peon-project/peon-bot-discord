d2479d32a9bcb48524dae4b99cd8bbc881b86143
Umlatt <theplacetosendmemail@gmail.com>
2022-05-30 21:00:56 +0000

    Added specific server get

--- app/modules/peon_orc_api.py
+++ app/modules/peon_orc_api.py
@@ -4,8 +4,14 @@ import requests
 
 # Services Get users in a certain group
 def getServers(url, api_key):
-    logging.debug('[getActiveServers]')
+    logging.debug('[getAllServers]')
     url = f"{url}/api/1.0/servers"
+    headers = { 'Accept': 'application/json', 'X-Api-Key': api_key }
+    return (requests.get(url, headers=headers)).json()
+
+def getServer(url, api_key, server_uid):
+    logging.debug('[getServer]')
+    url = f"{url}/api/1.0/server/get/{server_uid}"
     headers = { 'Accept': 'application/json', 'X-Api-Key': api_key }
     return (requests.get(url, headers=headers)).json()
 
