520bb41d1a580f50604a884da262ba53e8404b69
Umlatt <theplacetosendmemail@gmail.com>
2022-05-30 19:54:27 +0000

    Added warparties scan to file

--- app/main.py
+++ app/main.py
@@ -6,14 +6,12 @@ from discord.ext import commands
 from dotenv import load_dotenv
 from modules.peon_orc_api import *
 from modules.messaging import *
-from modules import project_path, settings
+from modules import project_path, getWarParties
 
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-
 bot = commands.Bot(command_prefix='!')
-
 usageText = (open(f"{project_path}/documents/help.md", "r")).read()
 
 @bot.event
@@ -25,18 +23,23 @@ async def on_ready():
                 await channel.send(f" has connected to the server.\n *Type ``!usage`` for furthur information*")
     print(f'[{bot.user.name}] has connected to Discord!')
 
-
 @bot.command(name='poke')
 async def poke(ctx):
     await ctx.send(f"*{quote('hello')}*")
 
 @bot.command(name='getall')
 async def getAll(ctx):
-    serverString="```c"
-    for server in getServers("http://dockerdev.za.cloudlet.cloud:5000","my-super-secret-api-key")["servers"]:
-        serverString = f"{serverString}\n{server['game_uid']}.{server['servername']}"
-    serverString = f"{serverString}\n```"
-    await ctx.send(serverString)
+    warparties = getWarParties()
+    if warparties != "EMPTY":
+        response=f"*\'{quote('hello')}\'*\n"
+        for warparty in warparties:
+            response+=f"**{warparty['name']}**\n```c"
+            for server in getServers(warparty['url'],warparty['key'])["servers"]:
+                response += f"\n{server['game_uid']}.{server['servername']}\t[{server['container_state']}]"
+            response += "\n```"
+    else:
+        response = errorMessage('none','register')
+    await ctx.send(response)
 
 @bot.command(name='register')
 async def register(ctx, *args):
@@ -49,4 +52,5 @@ async def register(ctx, *args):
 async def usage(ctx):
     await ctx.send(usageText)
 
+####### MAIN 
 bot.run(TOKEN)
