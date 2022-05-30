da861476e57a5a2f2409d43550281b798761f8ae
Umlatt <theplacetosendmemail@gmail.com>
2022-05-30 18:47:09 +0000

    Renamed file

--- app/modules/peon_orc_api.py
+++ app/modules/peon_orc_api.py
@@ -7,7 +7,9 @@ def getServers(url, api_key):
     logging.debug('[getActiveServers]')
     url = f"{url}/api/1.0/servers"
     headers = { 'Accept': 'application/json', 'X-Api-Key': api_key }
-    return (requests.get(url, headers=headers,)).json()
+    return (requests.get(url, headers=headers)).json()
+
+######## MAIN - FOR TEST PURPOSES
 
 if __name__ == "__main__":
     print(getServers("http://dockerdev.za.cloudlet.cloud:5000","my-super-secret-api-key"))
