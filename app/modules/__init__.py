3b791d1176727bf46955d9cd78478a0158c334ee
Umlatt <theplacetosendmemail@gmail.com>
2022-05-30 19:54:10 +0000

    Added get warparties from file

--- app/modules/__init__.py
+++ app/modules/__init__.py
@@ -1,5 +1,6 @@
 import json
 import os
+import logging
 
 project_path = "/".join(((os.path.dirname(__file__)).split("/"))[:-1])
 
@@ -7,3 +8,12 @@ project_path = "/".join(((os.path.dirname(__file__)).split("/"))[:-1])
 settings = json.load(open(f"{project_path}/settings.json", 'r'))
 # Container prefix
 prefix = "peon.warcamp."
+
+def getWarParties():
+    try:
+        logging.debug("Loading warcamps file") 
+        return json.load(open(f"{project_path}/warparties.json", 'r'))
+    except:
+        logging.debug("No warcamp file found. Creating one")
+        open(f"{project_path}/warparties.json", 'a').close()
+        return "EMPTY"
