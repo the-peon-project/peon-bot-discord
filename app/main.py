d4046d26abfc8b5c5a48f8fdd0f74529f11e2ded
Umlatt <theplacetosendmemail@gmail.com>
2022-05-31 13:21:23 +0000

    Formatting

--- app/main.py
+++ app/main.py
@@ -10,6 +10,7 @@ from modules import project_path, devMode
 
 bot = commands.Bot(command_prefix='!')
 
+
 @bot.event
 async def on_ready():
     channels = bot.get_all_channels()
@@ -19,53 +20,60 @@ async def on_ready():
                 await channel.send(f" has connected to the server.\n *Type ``!usage`` for furthur information*")
     logging.info(f'[{bot.user.name}] has connected to Discord!')
 
+
 @bot.command(name='poke')
 async def poke(ctx):
     logging.debug(f'Poke requested')
     await ctx.send(f"*{quote('hello')}*")
 
+
 @bot.command(name='getall')
 async def getAll(ctx):
     peon_orchestrators = getPeonOrchestrators()
     if peon_orchestrators != "EMPTY":
-        response=f"*\'{quote('hello')}\'*\n"
+        response = f"*\'{quote('hello')}\'*\n"
         for orchestrator in peon_orchestrators:
-            response+=f"**{orchestrator['name']}**\n```c"
-            for server in getServers(orchestrator['url'],orchestrator['key'])["servers"]:
+            response += f"**{orchestrator['name']}**\n```c"
+            for server in getServers(orchestrator['url'], orchestrator['key'])["servers"]:
                 response += f"\n{server['game_uid']}.{server['servername']}\t[{server['container_state']}]"
             response += "\n```"
     else:
-        response = errorMessage('none','register')
+        response = errorMessage('none', 'register')
     await ctx.send(response)
+
 
 @bot.command(name='get')
 async def get(ctx, *args):
-    await ctx.send(serverActions('get',args))
+    await ctx.send(serverActions('get', args))
+
 
 @bot.command(name='start')
 async def start(ctx, *args):
-    await ctx.send(serverActions('start',args))
+    await ctx.send(serverActions('start', args))
+
 
 @bot.command(name='stop')
 async def stop(ctx, *args):
-    await ctx.send(serverActions('stop',args))
+    await ctx.send(serverActions('stop', args))
+
 
 @bot.command(name='restart')
 async def restart(ctx, *args):
-    await ctx.send(serverActions('restart',args))
+    await ctx.send(serverActions('restart', args))
+
 
 @bot.command(name='register')
 async def register(ctx, *args):
     print("Server registration requested.")
     if len(args) != 3:
-        await ctx.send(errorMessage('parameterCount','register'))
+        await ctx.send(errorMessage('parameterCount', 'register'))
 
 
 @bot.command(name='usage')
 async def usage(ctx):
     await ctx.send(usageText)
 
-####### MAIN
+# MAIN
 if __name__ == "__main__":
     if devMode:
         load_dotenv(f"{project_path}/dev/.env")
@@ -73,6 +81,6 @@ if __name__ == "__main__":
     else:
         TOKEN = os.environ['DISCORD_TOKEN']
     usageText = (open(f"{project_path}/documents/help.md", "r")).read()
-    logging.basicConfig(filename='/var/log/peon/bot.discord.log', filemode='a', format='%(asctime)s %(thread)d [%(levelname)s] - %(message)s', level=logging.DEBUG)
+    logging.basicConfig(filename='/var/log/peon/bot.discord.log', filemode='a',
+                        format='%(asctime)s %(thread)d [%(levelname)s] - %(message)s', level=logging.DEBUG)
     logging.debug(bot.run(TOKEN))
-    
