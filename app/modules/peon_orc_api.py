67798106df1aaff210c8d28744c848f2f696b2db
Umlatt <theplacetosendmemail@gmail.com>
2022-07-03 10:12:00 +0000

    Initial nataural language rework (BEFORE FOR OLD)

--- app/modules/peon_orc_api.py
+++ app/modules/peon_orc_api.py
@@ -5,6 +5,7 @@ import logging
 import requests
 from modules import getPeonOrchestrators, settings
 from modules.messaging import *
+import re
 
 
 def getServersAll(peon_orchestrators):
@@ -19,7 +20,7 @@ def getServersAll(peon_orchestrators):
 
 # Services Get users in a certain group
 def getServers(url, api_key):
-    logging.debug('[getAllServers]')
+    logging.debug('[getServers]')
     url = f"{url}/api/1.0/servers"
     headers = { 'Accept': 'application/json', 'X-Api-Key': api_key }
     return (requests.get(url, headers=headers)).json()
@@ -32,13 +33,56 @@ def serverAction(url, api_key, server_uid, action):
         return (requests.get(url, headers=headers)).json()
     return (requests.put(url, headers=headers)).json()
 
+# Step 1 - Test for datetime (and remove from list)          ^(\d{4})\W(\d{2})\W(\d{2})\.(\d{2})[:h](\d{2})$    CCYY/MM/DD.HH:MM
+# Step 2 - Test for time (and remove from list)              ^(\d{2})[:h](\d{2})$ HH:MM
+# Step 3 - Test for interval (and remove from list)          ^\d+\D$   
+# Step 3 - Get name
+# 
+# interval match 
+# date match 
+
+def lookForRegexInArgs(regex,args):
+    try:
+        for argument in args:
+            match = re.search(regex, argument)
+            if match:
+                print (f"{match[0]}")
+                return match[0]
+    except:
+        return None
+
+def getParametersFromArgs(args):
+    if len(args) == 0:
+        return {"error" : "No arguments supplied"}
+    # Interval
+    arg_datetime = lookForRegexInArgs("^(\d{4})\W(\d{2})\W(\d{2})\.(\d{2})[:h](\d{2})$",args)
+    if arg_datetime: args.remove(arg_datetime)
+    arg_time = lookForRegexInArgs("^(\d{4})\W(\d{2})\W(\d{2})\.(\d{2})[:h](\d{2})$",args)
+    if arg_time: args.remove(arg_time)
+    arg_interval = lookForRegexInArgs("^\d+\D$",args)
+    if arg_interval: args.remove(arg_interval)
+
+
 def serverActions(action,args):
     peon_orchestrators = getPeonOrchestrators()
-    if len(args) != 2:
-        response = errorMessage('parameterCount',action)
-    elif peon_orchestrators == "EMPTY":
-        response = errorMessage('none','register')
-    elif not ([orchestrator for orchestrator in peon_orchestrators if args[0] == orchestrator['name']]):
+    # STEP 1: Check that there are some registered orchestrators
+    if len(peon_orchestrators) == 1:
+            arg_orchestrator = peon_orchestrators[0]["name"]
+    elif len(peon_orchestrators) == 0:
+        return errorMessage('orc.none',action)
+    # STEP 2: Get list of servers on orchestrators
+    ## SCALE ISSUE: If there are lots of Orcs, it could take time (so, if people end up using this, rewrite)
+    server_list = []
+    server = {}
+    for orchestrator in peon_orchestrators:
+        servers = getServers(orchestrator['url'], orchestrator['key'])['servers']
+        for server in servers:
+            server['orchestrator'] = orchestrator['name']
+        server_list.extend(servers)
+    return ('Bot is in DEV mode')
+
+
+    '''if not ([orchestrator for orchestrator in peon_orchestrators if args[0] == orchestrator['name']]):
         response = errorMessage('orc.dne', action)
     else:
         orchestrator = ([orchestrator for orchestrator in peon_orchestrators if args[0] == orchestrator['name']])[0]
@@ -68,6 +112,7 @@ def serverActions(action,args):
             else:
                 serverAction(orchestrator['url'],orchestrator['key'],args[1],action)
     return response
+    '''
 
 ######## MAIN - FOR TEST PURPOSES
 
