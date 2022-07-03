6965de9c75e817f0d001e93a0ad264f0ed1f6864
Umlatt <theplacetosendmemail@gmail.com>
2022-07-03 12:30:15 +0000

    Improved legebility

--- app/modules/messaging.py
+++ app/modules/messaging.py
@@ -12,5 +12,5 @@ def errorMessage(type="undefined",command="undefined"): # Add error header
     errorString = f"*\'{quote('nok')}\'*\n"
     errorString += f"*{messages['type'][type]}*\n" # Add error type
     errorString += f"```yaml\n{messages['sample'][command]}\n```" # Add help sample
-    errorString += f"* \*Optional (required if there are multple servers, on different systems, with the same server name)\n$Optional (if you wish to set an auto-stop)*" # Add help sample
+    errorString += f"*Optional (required if there are multple servers, on different systems, with the same server name)\n**Optional (if you wish to set an auto-stop)*" # Add help sample
     return errorString
