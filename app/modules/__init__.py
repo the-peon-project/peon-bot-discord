f16d44878853541d7b130a44634d0d1fcb834ed3
umlatt <theplacetosendmemail@gmail.com>
2023-05-23 17:57:12 +0000

    Removed dev & added error handling

--- app/modules/__init__.py
+++ app/modules/__init__.py
@@ -9,20 +9,13 @@ project_path = "/".join(((os.path.dirname(__file__)).split("/"))[:-1])
 # Configuration file
 settings = json.load(open(f"{project_path}/settings.json", 'r'))
 
-usageText = (open(f"{project_path}/config/documents/help.md", "r")).read()
+usage_text = (open(f"{project_path}/config/documents/help.md", "r")).read()
 
-def dev_mode():
-    if os.path.isdir(f"{project_path}/dev"):
-        logging.warn("DEV MODE ENABLED")
-        return True
-    else:
-        return False
-
-def get_peon_orchestrators():
+def get_peon_orcs():
     try:
         logging.debug("Loading orchestrators file") 
-        return json.load(open(f"{project_path}/config/peon.orchestrators.json", 'r'))
-    except:
+        return {"status" : "success" , "data" : json.load(open(f'{project_path}/config/peon.orchestrators.json', 'r'))}
+    except Exception as e:
         logging.debug("No war orchestrators file found. Creating one")
         open(f"{project_path}/config/peon.orchestrators.json", 'a').close()
-        return "EMPTY"
+        return {"status" : "error", "info" : f"{e}"}
