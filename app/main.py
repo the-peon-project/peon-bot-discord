ecf229ae0166a411f9e3972285ff36c845ac7630
umlatt <theplacetosendmemail@gmail.com>
2023-06-07 15:18:56 +0000

    Added first parse to allow channel control

--- app/main.py
+++ app/main.py
@@ -4,20 +4,22 @@ import re
 import logging
 import discord
 from discord.ext import commands
+from modules import usage_text, settings, build_payload
 from modules.peon_orc_api import *
 from modules.messaging import *
-from modules import project_path, usage_text, settings
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!',intents=intents)
+
+control_channel='peon'
 
 @bot.event
 async def on_ready():
     channels = bot.get_all_channels()
     for channel in channels:
         if re.search('text', str(channel.category), re.IGNORECASE):
-            if re.search('bot', str(channel.name), re.IGNORECASE):
+            if re.search(control_channel, str(channel.name), re.IGNORECASE):
                 await channel.send(" has connected to the server.\n *Type ``!usage`` for furthur information*")
     logging.info(f'[{bot.user.name}] has connected to Discord!')
 
@@ -29,40 +31,41 @@ async def poke(ctx):
 @bot.command(name='getall',aliases=settings["aliases"]["getall"])
 async def get_all(ctx):
     logging.debug("Servers & hosts \'get\' requested")
-    if "success" in ( peon_orchestrators := get_peon_orcs())['status']: # type: ignore
-        response = get_servers_all(peon_orchestrators['data'])
+    if ctx.channel.name == control_channel:
+        if "success" in ( peon_orchestrators := get_peon_orcs())['status']: # type: ignore
+            response = get_servers_all(peon_orchestrators['data'])
+        else:
+            response = error_message('none', 'register')
     else:
-        response = error_message('none', 'register') 
+        response = error_message('unauthorized', 'auth')
     await ctx.send(response)
 
 @bot.command(name='get',aliases=settings["aliases"]["get"])
 async def get(ctx, *args):
-    logging.debug(f"Server get requested - {args} ")
+    args = build_payload(channel_control=control_channel,channel_request=ctx.channel.name, args=args)
+    logging.debug(f"Server GET requested - {args} ")
     await ctx.send(server_actions('get', args))
 
 @bot.command(name='start',aliases=settings["aliases"]["start"])
 async def start(ctx, *args):
-    logging.info(f"Server start requested - {args} ")
+    logging.info(f"Server START requested - {args} ")
     await ctx.send(server_actions('start', args))
 
 @bot.command(name='stop',aliases=settings["aliases"]["stop"])
 async def stop(ctx, *args):
-    logging.info(f"Server stop requested - {args} ")
+    logging.info(f"Server STOP requested - {args} ")
     await ctx.send(server_actions('stop', args))
-
 
 @bot.command(name='restart',aliases=settings["aliases"]["restart"])
 async def restart(ctx, *args):
-    logging.info(f"Server restart requested - {args} ")
+    logging.info(f"Server RESTART requested - {args} ")
     await ctx.send(server_actions('restart', args))
-
 
 @bot.command(name='register',aliases=settings["aliases"]["register"])
 async def register(ctx, *args):
-    logging.info("Server registration requested.")
+    logging.info("Server REGISTRATION requested.")
     if len(args) != 3:
         await ctx.send(error_message('parameterCount', 'register'))
-
 
 @bot.command(name='usage',aliases=settings["aliases"]["usage"])
 async def usage(ctx):
@@ -71,8 +74,11 @@ async def usage(ctx):
 @bot.command(name='clear',aliases=settings["aliases"]["clear"])
 @commands.has_permissions(manage_messages=True)
 async def clear(ctx, amount: int):
-    await ctx.channel.purge(limit=amount + 1)
-    await ctx.send(f"{amount} message(s) got deleted")
+    if ctx.channel.name == control_channel:
+        await ctx.channel.purge(limit=amount + 1)
+        await ctx.send(f"{amount} message(s) got deleted")
+    else:
+        await ctx.send(error_message('unauthorized', 'auth'))
 
 # MAIN
 if __name__ == "__main__":
