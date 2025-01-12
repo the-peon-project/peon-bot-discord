e673d5c04145d76a32f32c1f235a857a427866ad
Umlatt <theplacetosendmemail@gmail.com>
2025-01-12 12:57:45 +0000

    Reworked filtering

--- app/modules/peon_orc_api.py
+++ app/modules/peon_orc_api.py
@@ -67,6 +67,7 @@ def server_action(url, api_key, server_uid, action, timer={}):
     return (requests.put(url, headers=headers,json=timer)).json()
 
 def server_actions(action,args):
+    logging.debug("STEP 1 - Check for active orchestrators")
     if len(args) == 0: return { "status" : "error", "err_code" : "srv.param", "command" : action}
     args_old = list(args)
     args = []
@@ -75,12 +76,12 @@ def server_actions(action,args):
     # STEP 1: Check that there are some registered orchestrators
     if 'error' in (result := get_peon_orcs())['status']: return { "status" : "error", "err_code" : "orc.none", "command" : action}
     peon_orchestrators = result['data']
-    #STEP 2: Get argument information
+    #STEP 2: Get argument information\
+    logging.debug("STEP 2 - Check for schedule data")
     arg_datetime = look_for_regex_in_args("^(\d{4})\W(\d{2})\W(\d{2})\.(\d{2})[:h](\d{2})$",args)
     if arg_datetime: args.remove(arg_datetime)
     arg_time = look_for_regex_in_args("^(\d{2})[:h](\d{2})$",args)
     if arg_time: args.remove(arg_time)
-    
     arg_interval = look_for_regex_in_args("^\d+.?$",args) # Look for a string of type D..DC (e.g. 5m, 20h, 3d)
     time_unit=""
     if not arg_interval:
@@ -91,6 +92,7 @@ def server_actions(action,args):
     
     if len(args) < 1: return { "status" : "error", "err_code" : "srv.param", "command" : action}
     # STEP 3: Get list of servers on orchestrators
+    logging.debug("STEP 3 - Collect all servers on all orchestrators")
     ## SCALE ISSUE: If there are lots of Orcs, it could take time (so, if people end up using this, rewrite). Then we need a local DB
     server_list = []
     server = {}
@@ -104,46 +106,47 @@ def server_actions(action,args):
             logging.warning(f"Host {orchestrator} is unavailable.")
     if len(server_list) == 0: return { "status" : "error", "err_code" : "orc.notavailable", "command" : action}
     # STEP 4: Try and find server with remaining arg/s
-    matched_server_list = []
+    logging.debug("STEP 3 - Filter by [servername]")
+    servers_matching_servername = []
     for server in server_list:
         arg_servername = look_for_regex_in_args(server['servername'].lower(),args)
         if arg_servername:
-            matched_server_list.append(server)
-            args.remove(arg_servername)
-            break
-    if len(matched_server_list) == 0: return { "status" : "error", "err_code" : "srv.dne", "command" : action}
-    elif len(matched_server_list) > 1:
-        server_list = matched_server_list
-        matched_server_list = []
+            logging.debug(f'{server}')
+            servers_matching_servername.append(server)
+    server_list = servers_matching_servername
+    if len(server_list) == 0: return { "status" : "error", "err_code" : "srv.dne", "command" : action}
+    elif len(server_list) > 1:
+        logging.debug("STEP 5 - Filter by [game_uid]")
+        servers_matching_gameuid = []
         for server in server_list:
             arg_gameuid = look_for_regex_in_args(server['game_uid'].lower(),args)
             if arg_gameuid:
-                matched_server_list.append(server)
-                args.remove(arg_gameuid)
-                break
-    if len(matched_server_list) == 0: return { "status" : "error", "err_code" : "srv.dne", "command" : action}
-    elif len(matched_server_list) > 1:
-        server_list = matched_server_list
-        matched_server_list = []
-        for server in server_list:
-            arg_orchestrator = look_for_regex_in_args(server['orchestrator'].lower(),args)
-            if arg_orchestrator:
-                matched_server_list.append(server)
-                args.remove(arg_orchestrator)
-                break
-    if len(matched_server_list) == 0: return { "status" : "error", "err_code" : "srv.dne", "command" : action}
-    elif len(matched_server_list) > 1: return { "status" : "error", "err_code" : "srv.param", "command" : action}
-    game_uid = matched_server_list[0]['game_uid']
-    servername = matched_server_list[0]['servername']
-    serveruid=f"{game_uid}.{servername}"
-    
-    orchestrator = next((orc for orc in peon_orchestrators if orc['name'] == matched_server_list[0]['orchestrator']), None)
+                logging.debug(f'{server}')
+                servers_matching_gameuid.append(server)
+        server_list = servers_matching_gameuid
+        logging.debug("STEP 6 - Filter by Orchestrator")
+        if len(server_list) == 0: return { "status" : "error", "err_code" : "srv.dne", "command" : action}
+        elif len(server_list) > 1:
+            matched_orchestrator_list = []
+            for server in server_list:
+                arg_orchestrator = look_for_regex_in_args(server['orchestrator'].lower(),args)
+                if arg_orchestrator:
+                    matched_orchestrator_list.append(server)
+                    break
+            server_list = matched_orchestrator_list
+            if len(server_list) > 1: return { "status" : "error", "err_code" : "srv.notexplicit", "command" : action}
+    server = server_list[0]
+    args.remove(server['servername'])
+    if server['game_uid'] in args: args.remove(server['game_uid'])
+    logging.debug(f"STEP 7 - Trigger appropriate action on server")
+    serveruid=f"{server['game_uid']}.{server['servername']}"
+    orchestrator = next((orc for orc in peon_orchestrators if orc['name'] == server['orchestrator']), None)
     # STEP 5: Trigger action on server
     data = server_action(orchestrator['url'],orchestrator['key'],serveruid,'get')
     if "error" in data:
         response = { "status" : "error", "err_code" : "srv.action.error", "command" : action}
     else:
-        response =f"Orc **{action}** warcamp **{servername}** in {orchestrator['name'].upper()}."
+        response =f"Orc **{action}** warcamp **{server['servername']}** in {orchestrator['name'].upper()}."
         if action == 'get':
             response += "```yaml\n{0:<15}: {1}\n{2:<15}: {3}\n{4:<15}: {5}\n{6:<15}: {7}\n".format("Warcamp",data["servername"],"Type",data['game_uid'],"Peon State",(data["server_state"].lower()),"Description",data["description"])
             try:
@@ -186,6 +189,7 @@ def server_actions(action,args):
             elif arg_interval:
                 timer = { "interval" : f"{arg_interval}" }
                 response += f"\n\n\t:alarm_clock: Warcamp will shut down in ``{arg_interval}``."
+            logging.debug("STEP 8")
             if len(args) == 1: # Only the permissions arg should be left
                 server_action(orchestrator['url'], orchestrator['key'], serveruid, action, timer)
             else:
