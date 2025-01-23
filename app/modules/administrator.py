f1a4eca62868e6692eb9fe40b34cd9d3ee11021d
Umlatt <theplacetosendmemail@gmail.com>
2025-01-23 18:21:30 +0000

    renamed files

--- app/modules/administrator.py
+++ app/modules/administrator.py
@@ -3,7 +3,6 @@ import discord
 from . import *
 from .orchestrator import *
 
-
 # Orchestrator Registration Modal
 class RegisterModal(discord.ui.Modal, title='Register Orchestrator'):
     orchestrator_name = discord.ui.TextInput(
@@ -27,7 +26,6 @@ class RegisterModal(discord.ui.Modal, title='Register Orchestrator'):
         min_length=1,
         max_length=100
     )
-
     async def on_submit(self, interaction: discord.Interaction):
         logging.info(f"Orchestrator registration: {self.orchestrator_name.value} at {self.orchestrator_url.value}")
         if (response := register_peon_orc(self.orchestrator_name.value, self.orchestrator_url.value, self.orchestrator_api_key.value))["status"] == "success":
@@ -40,40 +38,62 @@ class RegisterModal(discord.ui.Modal, title='Register Orchestrator'):
             embed = build_card_err(err_code="bad.code", command="bad.code", permission="user", note=response['info'])
         await interaction.response.send_message(embed=embed)
 
+class ConfirmView(discord.ui.View):
+    def __init__(self, orchestrator_name: str):
+        super().__init__()
+        self.orchestrator_name = orchestrator_name
+
+    @discord.ui.button(label="Confirm", style=discord.ButtonStyle.danger)
+    async def confirm(self, interaction: discord.Interaction, button: discord.ui.Button):
+        # TODO: Add actual deregister_peon_orc() function call here
+        embed = build_card(
+            title="Deregistration Request",
+            message=f"Orchestrator '{self.orchestrator_name}' has been deregistered."
+        )
+        await interaction.response.send_message(embed=embed)
+
+    @discord.ui.button(label="Cancel", style=discord.ButtonStyle.secondary)
+    async def cancel(self, interaction: discord.Interaction, button: discord.ui.Button):
+        embed = build_card(
+            title="Deregistration Request",
+            message="Deregistration cancelled."
+        )
+        await interaction.response.send_message(embed=embed)
+
+class DeregisterSelect(discord.ui.Select):
+    def __init__(self):
+        options = []
+        if (orchestrators := get_peon_orcs())["status"] == "success":
+            options = [
+                discord.SelectOption(label=orc['name'], value=orc['name'])
+                for orc in orchestrators['data']
+            ]
+        super().__init__(
+            placeholder='Select an orchestrator to remove...',
+            min_values=1,
+            max_values=1,
+            options=options
+        )
+
+    async def callback(self, interaction: discord.Interaction):
+        view = ConfirmView(self.values[0])
+        await interaction.response.send_message(
+            f"Are you sure you want to deregister orchestrator '{self.values[0]}'?",
+            view=view,
+            ephemeral=True
+        )
+
 class RegisterButton(discord.ui.View):
     def __init__(self):
         super().__init__()
-
+        
     @discord.ui.button(label="Register Orchestrator", style=discord.ButtonStyle.primary)
     async def register_button(self, interaction: discord.Interaction, button: discord.ui.Button):
         modal = RegisterModal()
         await interaction.response.send_modal(modal)
-        
-
-
-class DeRegisterModal(discord.ui.Modal, title='Deregister Orchestrator'):
-    orchestrator_name = discord.ui.TextInput(
-        label='Orchestrator Name',
-        placeholder='Enter an orchestrator name...',
-        required=True,
-        min_length=3,
-        max_length=50
-    )
-
-    async def on_submit(self, interaction: discord.Interaction):
-        logging.info(f"Orchestrator de-registration: {self.orchestrator_name.value} at {self.orchestrator_url.value}")
-        if (response := register_peon_orc(self.orchestrator_name.value, self.orchestrator_url.value, self.orchestrator_api_key.value))["status"] == "success":
-            response_string = f"Orchestrator: {self.orchestrator_name.value}\nURL: {self.orchestrator_url.value} was unregistered.\n```bash\n"
-            embed = build_card(title="Deregistration Request", message=response_string)
-        else:
-            embed = build_card_err(err_code="bad.code", command="bad.code", permission="user", note=response['info'])
-        await interaction.response.send_message(embed=embed)
-
-class DeregisterButton(discord.ui.View):
-    def __init__(self):
-        super().__init__()
 
     @discord.ui.button(label="De-Register Orchestrator", style=discord.ButtonStyle.primary)
     async def deregister_button(self, interaction: discord.Interaction, button: discord.ui.Button):
-        modal = DeRegisterModal()
-        await interaction.response.send_modal(modal)
+        view = discord.ui.View()
+        view.add_item(DeregisterSelect())
+        await interaction.response.send_message("Select an orchestrator to remove:", view=view, ephemeral=True)
