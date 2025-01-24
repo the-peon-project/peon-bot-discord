016cf81c845090587829b4ed5af1cc4ee1f827a3
Umlatt <theplacetosendmemail@gmail.com>
2025-01-24 21:48:28 +0000

    Added about & error handling

--- app/main.py
+++ app/main.py
@@ -43,15 +43,19 @@ async def on_ready():
 @commands.has_permissions(manage_messages=True)
 async def peon(ctx):
     await clean_channel(ctx.channel, bot.user,limit=20)
-    gameuid=(str(ctx.channel.name)).split('-')[0],
-    servername=(str(ctx.channel.name)).split('-')[1]
-    view = UserActions(gameuid=gameuid,servername=(servername))
-    embed = discord.Embed(
-        description=f"*{get_quote()}*",
-        color=discord.Color.blue()
-    )
-    embed.set_image(url=bot_image)
-    await ctx.channel.send(embed=embed, view=view)
+    try:
+        gameuid=(str(ctx.channel.name)).split('-')[0],
+        servername=(str(ctx.channel.name)).split('-')[1]
+    except:
+        gameuid = None
+        servername = None
+    if gameuid and servername:     
+        view = UserActions(gameuid=gameuid,servername=(servername))
+        embed = discord.Embed(description=f"*{get_quote()}*",color=discord.Color.blue())
+        embed.set_image(url=bot_image)
+        await ctx.channel.send(embed=embed, view=view)
+    else:
+        await ctx.send(embed=build_card(status='nok',message="This doesn't look like a warcamp, warchief!"))
 
 # Command: Clean channel
 @bot.command(name='clear',aliases=cmd_aliases["clear"])
@@ -61,6 +65,38 @@ async def clear(ctx, amount: int = 10):
     #args = identify_channel(channel_request=ctx.channel.name)
     # if args[0] == 'admin': await ctx.channel.purge(limit=amount + 1)
     # else: await ctx.send(embed=build_card_err(err_code="unauthorized",command="auth",permission=args[0]))
+
+@bot.command(name='about')
+async def get_plans(ctx):
+    logging.debug("'About' requested")
+    with open(f"/app/reference/{settings['language']}/about.md", "r") as file:
+        response = file.read()
+    response = response.replace('[BOT_VERSION]',os.environ.get('VERSION', '-.-.-'))
+    if (orchestrators := get_peon_orcs())['status'] == "success":
+        if orchestrators['data']:
+            orcstring = ""
+            for orc in orchestrators['data']:
+                if (orc_response := get_orchestrator_details(url=orc['url'],api_key=orc['key']))['status'] == "success":
+                    info = orc_response['data']
+                    orcstring += f"- Orchestrator: {orc['name']} [{info['version']}](<https://docs.warcamp.org/development/50_bot_discord/#release-notes>)\n"
+                else:
+                    orcstring += f"- Orchestrator: {orc['name']} [UNKNOWN](<https://docs.warcamp.org/development/50_bot_discord/#release-notes>)\n"
+            response = response.replace('[ORCHESTRATORS]',f"{orcstring}")
+    try:
+        file_contents = requests.get(games_url).text
+        servers = "### Supported Games\nBelow is a list of games that are currently supported by the PEON Project.\n"
+        for line in file_contents.splitlines():
+            if re.search('- \[x\]', line):
+                line = re.sub('- \[x\]', '- ', line)
+                line = re.sub('./guides/games/', 'https://docs.warcamp.org/guides/games/', line)
+                line = re.sub('.md', '', line)
+                servers += line + '\n'
+    except:
+        servers = ""
+    response = response.replace('[GAME_SERVERS]',servers)
+    embed = discord.Embed(description=response)
+    embed.set_image(url=bot_image) 
+    await ctx.send(embed=embed)
 
 # MAIN
 if __name__ == "__main__":
