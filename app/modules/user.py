2a84927cb8f166a430307e045d1e1192da13264f
Umlatt <theplacetosendmemail@gmail.com>
2025-01-24 21:13:54 +0000

    Functionized standard parts

--- app/modules/user.py
+++ app/modules/user.py
@@ -9,22 +9,20 @@ class UserActions(discord.ui.View):
         self.servername = servername
         super().__init__()
         
+    async def _handle_server_action(self, interaction: discord.Interaction, action: str):
+        username = str(interaction.user)
+        logging.info(f"Server {action.upper()} requested by {username} - {self.gameuid} - {self.servername}")
+        embed = build_card(
+            title=f"{action.capitalize()} Server",
+            message=f"{action.capitalize()} requested by @{username}"
+        )
+        await interaction.response.defer()
+        await interaction.channel.send(embed=embed)
+
     @discord.ui.button(label="Start", style=discord.ButtonStyle.primary)
     async def server_start(self, interaction: discord.Interaction, button: discord.ui.Button):  
-        username = str(interaction.user)
-        logging.info(f"Server START requested by {username} - {self.gameuid} - {self.servername}")
-        embed = build_card(
-            title="Start Server",
-            message=f"Start requested by {username}"
-        )
-        await interaction.response.send_message(embed=embed)
+        await self._handle_server_action(interaction, "start")
 
     @discord.ui.button(label="Stop", style=discord.ButtonStyle.danger)
     async def server_stop(self, interaction: discord.Interaction, button: discord.ui.Button):
-        username = str(interaction.user)
-        logging.info(f"Server STOP requested by {username} - {self.gameuid} - {self.servername}")
-        embed = build_card(
-            title="Stop Server",
-            message=f"Stop requested by {username}"
-        )
-        await interaction.response.send_message(embed=embed)
+        await self._handle_server_action(interaction, "stop")
