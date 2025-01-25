03aec788cc89a5d58ecd2c4685d2ab196c6c174f
Umlatt <theplacetosendmemail@gmail.com>
2025-01-25 19:07:24 +0000

    Added buttons and started get/info

--- app/modules/user.py
+++ app/modules/user.py
@@ -7,6 +7,7 @@ class UserActions(discord.ui.View):
     def __init__(self, gameuid, servername):
         self.gameuid = gameuid
         self.servername = servername
+        self.message_body = ""
         super().__init__(timeout=None) # This may cause a memory leak???
         
     async def _handle_server_action(self, interaction: discord.Interaction, action: str):
@@ -17,10 +18,31 @@ class UserActions(discord.ui.View):
         await interaction.response.defer()
         await interaction.channel.send(embed=embed)
 
-    @discord.ui.button(label="Start", style=discord.ButtonStyle.primary)
-    async def server_start(self, interaction: discord.Interaction, button: discord.ui.Button):  
+    @discord.ui.button(label="Start", style=discord.ButtonStyle.success, row=0)
+    async def server_start(self, interaction: discord.Interaction, button: discord.ui.Button):
         await self._handle_server_action(interaction, "start")
+        
+    @discord.ui.button(label="Restart", style=discord.ButtonStyle.secondary, row=0)
+    async def server_restart(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await self._handle_server_action(interaction, "restart")
 
-    @discord.ui.button(label="Stop", style=discord.ButtonStyle.danger)
+    @discord.ui.button(label="Stop", style=discord.ButtonStyle.danger, row=0)
     async def server_stop(self, interaction: discord.Interaction, button: discord.ui.Button):
         await self._handle_server_action(interaction, "stop")
+    
+    @discord.ui.button(label="Info", style=discord.ButtonStyle.secondary, row=1)
+    async def server_info(self, interaction: discord.Interaction, button: discord.ui.Button):  
+        if (response := server_actions(action='get',args=[self.gameuid,self.servername]))['status'] == 'success':
+            self.message_body = response['data']
+            logging.critical(f"Server info requested: {self.message_body}")
+            await self._handle_server_action(interaction, "info")
+        else:
+            await interaction.channel.send(embed=build_card(status='nok',message="Could not retrieve server information"))
+    
+    @discord.ui.button(label="Update", style=discord.ButtonStyle.secondary, row=1)
+    async def server_update(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await self._handle_server_action(interaction, "update")
+    
+    @discord.ui.button(label="About", style=discord.ButtonStyle.secondary, row=1)
+    async def server_about(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await self._handle_server_action(interaction, "about")
