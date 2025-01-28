d6eaeccfadc1d05ab6e3ea02f6f76beef2d4cf3d
Umlatt <theplacetosendmemail@gmail.com>
2025-01-28 13:02:19 +0000

    Improved indicators of action

--- app/modules/user.py
+++ app/modules/user.py
@@ -24,30 +24,24 @@ class UserActions(discord.ui.View):
         self.message_body = ""
         super().__init__(timeout=None)
         
-    def disable_all_buttons(self):
+    def _disable_all_buttons(self):
         for item in self.children:
             if isinstance(item, discord.ui.Button):
                 item.disabled = True
 
     async def _handle_server_action(self, interaction: discord.Interaction, action: str):
-        self.disable_all_buttons()
-        
-        # Show disabled buttons immediately with message
+        self._disable_all_buttons()
         await interaction.response.edit_message(view=self)
-        
         username = str(interaction.user)
         nickname = str(interaction.user.display_name)
         logging.info(f"Server {action.upper()} triggered by <@{username}> for {self.servername} ({self.gameuid})")
-        
-        # Handle API call
         response = server_actions(action=action, args=[self.gameuid, self.servername])
         if response['status'] == 'success':
             if action == 'get': message = response['data']
-            else: message = f"**{action.capitalize()}** server triggered by *@{nickname}*"
+            else: message = f"Server **{action.upper()}** triggered by *@{nickname}*"
             embed = build_card(status='ok', message=message)
         else: embed = build_card(status='nok', message=response.get('err_code', "Could not process server request"))
-        
-        await interaction.followup.send(embed=embed)
+        await interaction.channel.send(embed=embed)
         await remove_interactions(interaction)
 
     @discord.ui.button(label="Start", style=discord.ButtonStyle.success, row=0)
@@ -68,21 +62,19 @@ class UserActions(discord.ui.View):
     
     @discord.ui.button(label="Update", style=discord.ButtonStyle.secondary, row=1)
     async def server_update(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.defer()
+        self._disable_all_buttons()
+        await interaction.response.edit_message(view=self)
         view = discord.ui.View()
         view.add_item(UpdateModeSelect(self.gameuid, self.servername))
-        embed = discord.Embed(
-            description="Select an update mode:",
-            color=discord.Color.blue()
-        )
-        embed.set_thumbnail(url=bot_thumbnail)
-        await interaction.channel.send(embed=embed, view=view)
+        embed = discord.Embed(description="Select an update mode:",color=discord.Color.blue())
+        embed.set_thumbnail(url=f"{bot_thumbnail}")  # Add size parameter
+        await interaction.followup.send(embed=embed, view=view)
 
     @discord.ui.button(label="About", style=discord.ButtonStyle.secondary, row=1)
     async def server_about(self, interaction: discord.Interaction, button: discord.ui.Button):
-        self.disable_all_buttons()
+        self._disable_all_buttons()
         await interaction.response.edit_message(content="*Getting about info...*", view=self)
-        message = await interaction.followup.send(embed=build_about_card())
+        message = await interaction.channel.send(embed=build_about_card())
         await remove_interactions(interaction,keep=message.id)
 
 class UpdateModeSelect(discord.ui.Select):
@@ -90,39 +82,25 @@ class UpdateModeSelect(discord.ui.Select):
         self.gameuid = gameuid
         self.servername = servername
         options = [
-            discord.SelectOption(
-                label="Game Server",
-                value="server",
-                description="Update the game server files <default>"
-            ),
-            discord.SelectOption(
-                label="Server Container",
-                value="image",
-                description="Update the PEON container in which the game server runs."
-            ),
-            discord.SelectOption(
-                label="Complete Upgrade",
-                value="full",
-                description="Update both the game server & the Peon container"
-            )
+            discord.SelectOption(label="Game Server",value="server",description="Update the game server files <default>"),
+            discord.SelectOption(label="Server Container",value="image",description="Update the PEON container in which the game server runs."),
+            discord.SelectOption(label="Complete Upgrade",value="full",description="Update both the game server & the Peon container")
         ]
-        super().__init__(
-            placeholder='Select update mode...',
-            min_values=1,
-            max_values=1,
-            options=options
-        )
+        super().__init__(placeholder='Select update mode...',min_values=1,max_values=1,options=options)
+        self.disabled = False  # Add initial disabled state
 
+    def _disable_select(self):
+        self.disabled = True
+        self.view.stop()  # Stop the view to prevent further interactions
+        
     async def callback(self, interaction: discord.Interaction):
-        await interaction.response.defer()
+        self._disable_select()
+        await interaction.response.edit_message(view=self.view)
         view = UpdateConfirmView(self.values[0], self.gameuid, self.servername)
         selected_option = next(opt for opt in self.options if opt.value == self.values[0])
-        embed = discord.Embed(
-            description=f"Are you sure you want to perform a *{selected_option.label}* update?",
-            color=discord.Color.yellow()
-        )
-        embed.set_thumbnail(url=bot_thumbnail)
-        await interaction.channel.send(embed=embed, view=view)
+        embed = discord.Embed(description=f"Are you sure you want to perform a *{selected_option.label}* update?",color=discord.Color.yellow())
+        embed.set_thumbnail(url=f"{bot_thumbnail}")  # Add size parameter
+        await interaction.followup.send(embed=embed, view=view)
 
 class UpdateConfirmView(discord.ui.View):
     def __init__(self, mode: str, gameuid: str, servername: str):
@@ -130,16 +108,22 @@ class UpdateConfirmView(discord.ui.View):
         self.mode = mode
         self.gameuid = gameuid
         self.servername = servername
+        
+    def _disable_all_buttons(self):
+        for item in self.children:
+            if isinstance(item, discord.ui.Button):
+                item.disabled = True
 
     @discord.ui.button(label="Confirm", style=discord.ButtonStyle.danger)
     async def confirm(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.defer()
+        self._disable_all_buttons()
+        await interaction.response.edit_message(view=self)
         username = str(interaction.user)
         nickname = str(interaction.user.display_name)
-        logging.info(f"Server {(self.mode).upper()} UPDATE triggered by <@{username}> for {self.servername} ({self.gameuid})")
+        logging.info(f"Server **UPDATE ({(self.mode).upper()})** {self.mode} triggered by <@{username}> for {self.servername} ({self.gameuid})")
         response = server_actions(action='update', args=[self.gameuid, self.servername, self.mode])
-        if response['status'] == 'success': embed = build_card(status='ok',  message=f"**{(self.mode).upper()} upgrade** triggered by *@{nickname}*")
-        else: embed = build_card(status='nok', message="**{(self.mode).upper()} upgrade** triggered by *@{nickname}* FAILED")
+        if response['status'] == 'success': embed = build_card(status='ok',  message=f"Server **UPDATE ({(self.mode).upper()})** triggered by *@{nickname}*")
+        else: embed = build_card(status='nok', message="Server **UPDATE ({(self.mode).upper()})** triggered by *@{nickname}* FAILED")
         await interaction.channel.send(embed=embed)
         await remove_interactions(interaction)
 
